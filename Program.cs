fdbb5696fa8c08817f30e072661c92d47b68c8ac
Yosandi <elyosandireyes@gmail.com>
2022-06-02 01:45:21 -0400

    Projet finished

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Testing
+            Solutions.Exercice1(5, 5, 5);
+            Solutions.PringLine();
+
+            Solutions.Exercice2(5, 10, 10);
+            Solutions.PringLine();
+
+            Solutions.Exercice3(5, 5, 5);
+            Solutions.PringLine();
+
+            Solutions.Exercice4(5, 5, 5);
+            Solutions.PringLine();
+
+            Solutions.Exercice5(5, 5, 5);
+            Solutions.PringLine();
+
+            Solutions.Exercice6(5, 5, 5,5);
+            Solutions.PringLine();
+
+            Solutions.Exercice7(5, 5, 5, 5);
+            Solutions.PringLine();
+            #endregion
+
+        }
+    }
+}
---
+++ Solutions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicios
+{
+    static class Solutions
+    {
+        #region MainCode
+        public static void Exercice1(double a,double b,double c)
+        {
+            Console.WriteLine((a+(b/c)) / ((a/b)+c));
+        }
+
+        public static void Exercice2(double a,double b, double c)
+        {
+            Console.WriteLine((a+b+a/b)/c);
+        }
+
+        public static void Exercice3(double a, double b, double c)
+        {
+            Console.WriteLine((a/(a+b)) / (a/(a-b)));
+        }
+
+        public static void Exercice4(double a, double b,double c)
+        {
+            Console.WriteLine((a + (b / (a + b + b / c))) / (a + b/(c+a)));
+        }
+
+        public static void Exercice5(double a,double b,double c)
+        {
+            Console.WriteLine((a + b + c) / (a + b/c));
+        }
+
+        public static void Exercice6(double a , double b, double c,double d)
+        {
+            Console.WriteLine((a+b) + (c / (d*a)) / ((a+b) * (c/d)));
+        }
+
+        public static void Exercice7(double a, double b, double c, double d)
+        {
+            Console.WriteLine((a + b / c + d) / a);
+        }
+        #endregion
+        public static void PringLine()
+        {
+            Console.WriteLine("-----------------------------------------------------------------------------");
+        }
+    }
+}
